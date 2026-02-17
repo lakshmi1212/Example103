2edf67b47abca58bb4962ac6f23bc64d4f65941b
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-02-17 18:02:07 +0530

    Add or update file: test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,24 +1,21 @@
 import pytest
 from src.math_operations import add
 
-def test_add_positive_numbers():
+def test_add_integers():
     assert add(2, 3) == 5
-
-def test_add_negative_numbers():
-    assert add(-2, -3) == -5
-
-def test_add_mixed_sign_numbers():
-    assert add(-2, 3) == 1
-    assert add(2, -3) == -1
-
-def test_add_zero():
+    assert add(-1, 1) == 0
     assert add(0, 0) == 0
-    assert add(0, 5) == 5
-    assert add(5, 0) == 5
-
-def test_add_large_numbers():
-    assert add(10**6, 10**6) == 2 * 10**6
 
 def test_add_floats():
-    assert add(2.5, 3.1) == pytest.approx(5.6)
-    assert add(-2.5, 3.1) == pytest.approx(0.6)
+    assert add(2.5, 3.5) == 6.0
+    assert add(-1.2, 1.2) == 0.0
+    assert add(0.0, 0.0) == 0.0
+
+def test_add_mixed_types():
+    assert add(2, 3.5) == 5.5
+    assert add(-1.5, 2) == 0.5
+
+@pytest.mark.parametrize("a,b", [("2", 3), (None, 1), ([], {})])
+def test_add_invalid_types(a, b):
+    with pytest.raises(TypeError):
+        add(a, b)
